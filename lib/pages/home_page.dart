b3d33b213b1159d5b07121cc4f491b6b2ee5bbec
Your Name <you@example.com>
2024-09-19 15:48:19 +0300

    added delete button and if task is completed then checkmark is hidden, also added some UI features

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tamer_task/pages/add_task_page.dart';
 import 'package:tamer_task/pages/task_details.dart';
 import '../db/database_helper.dart';
+import '../models/status_enum.dart';
 import '../models/task_model.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -47,35 +48,74 @@ class _HomePageState extends State<HomePage> {
       ),
       body: _tasks.isEmpty
           ? Center(child: Text('No tasks available.'))
-          : ListView.builder(
-              itemCount: _tasks.length,
-              itemBuilder: (context, index) {
-                final task = _tasks[index];
-                return ListTile(
-                  leading:
-                      IconButton(onPressed: () {}, icon: Icon(Icons.image)),
-                  title: Text(task.title!),
-                  subtitle: Text(
-                    'Priority: ${task.priority.toString().split('.').last} - Status: ${task.status.toString().split('.').last}',
-                  ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.check),
-                    onPressed: () {
-                      _deleteTask(task.id!);
-                    },
-                  ),
-                  onTap: () async {
-                    final result = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                TaskDetails(currentTask: task)));
-                    if (result == true) {
-                      _loadTasks();
-                    }
-                  },
-                );
-              },
+          : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView.builder(
+                itemCount: _tasks.length,
+                itemBuilder: (context, index) {
+                  final task = _tasks[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0)),
+                      splashColor: task.status == Status.completed
+                          ? Colors.transparent
+                          : Colors.grey,
+                      tileColor: task.status == Status.completed
+                          ? Colors.lightGreen
+                          : Colors.transparent,
+                      leading:
+                          IconButton(onPressed: () {}, icon: Icon(Icons.image)),
+                      title: Text(task.title!),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Priority: ${task.priority.toString().split('.').last}',
+                          ),
+                          Text(
+                            'Status: ${task.status.toString().split('.').last}',
+                          ),
+                        ],
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          task.status == Status.completed
+                              ? SizedBox()
+                              : IconButton(
+                                  icon: Icon(Icons.check),
+                                  onPressed: () {
+                                    task.status = Status.completed;
+                                    _dbHelper.updateTaskStatus(task);
+                                    setState(() {
+                                      _loadTasks();
+                                    });
+                                  },
+                                ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              _deleteTask(task.id!);
+                            },
+                          )
+                        ],
+                      ),
+                      onTap: () async {
+                        final result = await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    TaskDetails(currentTask: task)));
+                        if (result == true) {
+                          _loadTasks();
+                        }
+                      },
+                    ),
+                  );
+                },
+              ),
             ),
     );
   }
