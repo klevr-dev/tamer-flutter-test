eddc6fcb0fa7d0d734de89c538ebd76f98e92524
Your Name <you@example.com>
2024-10-04 12:15:33 +0300

    light/dark mode

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,17 +16,36 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   final _dbHelper = DatabaseHelper();
   List<Task> _tasks = [];
   List<Task> _filteredTasks = [];
   List<Priority> selectedPriority = [];
   List<Status> selectedStatus = [];
 
+  late TabController _tabControllerUI;
+
+  ThemeMode themeMode = ThemeMode.system;
+
   @override
   void initState() {
     super.initState();
     _loadTasks();
+
+    _tabControllerUI = TabController(length: 2, vsync: this);
+    _tabControllerUI.index = 0;
+    _tabControllerUI.addListener(() {
+      if (_tabControllerUI.index == 0) {
+        setState(() {
+          themeMode = ThemeMode.light;
+        });
+      } else {
+        setState(() {
+          themeMode = ThemeMode.dark;
+        });
+      }
+    });
   }
 
   Future<void> _pickImage(Task t) async {
@@ -158,132 +177,163 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var groupedTasks = groupTasksByStatus(_filteredTasks);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'My Tasks',
-          style: TextStyle(fontSize: 32),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blue,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.grey,
+          )),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.grey[900],
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.grey[900],
         ),
-        actions: [
-          IconButton(
-            icon: Icon(size: 40, Icons.add),
-            onPressed: () async {
-              final result = await Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const AddTaskPage()));
-              if (result == true) {
-                _loadTasks();
-              }
-            },
-          ),
-          IconButton(
-            icon: Icon(size: 30, Icons.filter_alt),
-            onPressed: _showFilterMenu,
-          ),
-        ],
       ),
-      body: _filteredTasks.isEmpty
-          ? Center(child: Text('No tasks available.'))
-          : ListView(
-              children: [
-                for (Status status in groupedTasks.keys) ...[
-                  if (groupedTasks[status]!.isNotEmpty) ...[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        '${status.toString()} Tasks',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    for (var task in groupedTasks[status]!)
+      themeMode: themeMode,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'My Tasks',
+            style: TextStyle(fontSize: 32),
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(size: 40, Icons.add),
+              onPressed: () async {
+                final result = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const AddTaskPage()));
+                if (result == true) {
+                  _loadTasks();
+                }
+              },
+            ),
+            IconButton(
+              icon: Icon(size: 30, Icons.filter_alt),
+              onPressed: _showFilterMenu,
+            ),
+          ],
+          bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            indicatorSize: TabBarIndicatorSize.tab,
+            labelColor: Colors.black,
+            controller: _tabControllerUI,
+            dividerColor: Colors.transparent,
+            indicator: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(32.0)),
+            tabs: [Tab(text: "Light"), Tab(text: "Dark")],
+          ),
+        ),
+        body: _filteredTasks.isEmpty
+            ? Center(child: Text('No tasks available.'))
+            : ListView(
+                children: [
+                  for (Status status in groupedTasks.keys) ...[
+                    if (groupedTasks[status]!.isNotEmpty) ...[
                       Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: ListTile(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16.0)),
-                          splashColor: task.status == Status.completed
-                              ? Colors.transparent
-                              : Colors.grey,
-                          tileColor: task.status == Status.completed
-                              ? Colors.lightGreen
-                              : Colors.transparent,
-                          leading: SizedBox(
-                            width: 60,
-                            height: 60,
-                            child: IconButton(
-                                padding: EdgeInsets.zero,
-                                onPressed: () {
-                                  _pickImage(task);
-                                },
-                                icon: task.imagePath == null
-                                    ? Icon(
-                                        Icons.image,
-                                        size: 40,
-                                      )
-                                    : ClipRRect(
-                                        borderRadius: BorderRadius.circular(16),
-                                        child: Image.file(
-                                          File(task.imagePath!),
-                                          fit: BoxFit.cover,
-                                          width: 60,
-                                          height: 60,
-                                        ),
-                                      )),
-                          ),
-                          title: Text(task.title!),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Priority: ${task.priority.toString()}',
-                              ),
-                              Text(
-                                'Status: ${task.status.toString()}',
-                              ),
-                            ],
-                          ),
-                          trailing: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              task.status == Status.completed
-                                  ? SizedBox()
-                                  : IconButton(
-                                      icon: Icon(Icons.check),
-                                      onPressed: () {
-                                        task.status = Status.completed;
-                                        _dbHelper.updateTaskStatus(task);
-                                        setState(() {
-                                          _loadTasks();
-                                        });
-                                      },
-                                    ),
-                              IconButton(
-                                icon: Icon(Icons.delete),
-                                onPressed: () {
-                                  _deleteTask(task.id!);
-                                  setState(() {
-                                    _loadTasks();
-                                  });
-                                },
-                              )
-                            ],
-                          ),
-                          onTap: () async {
-                            final result = await Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        TaskDetails(currentTask: task)));
-                            if (result == true) {
-                              _loadTasks();
-                            }
-                          },
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          '${status.toString()} Tasks',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
                         ),
                       ),
-                  ]
+                      for (var task in groupedTasks[status]!)
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16.0)),
+                            splashColor: task.status == Status.completed
+                                ? Colors.transparent
+                                : Colors.grey,
+                            tileColor: task.status == Status.completed
+                                ? Colors.lightGreen
+                                : Colors.transparent,
+                            leading: SizedBox(
+                              width: 60,
+                              height: 60,
+                              child: IconButton(
+                                  padding: EdgeInsets.zero,
+                                  onPressed: () {
+                                    _pickImage(task);
+                                  },
+                                  icon: task.imagePath == null
+                                      ? Icon(
+                                          Icons.image,
+                                          size: 40,
+                                        )
+                                      : ClipRRect(
+                                          borderRadius:
+                                              BorderRadius.circular(16),
+                                          child: Image.file(
+                                            File(task.imagePath!),
+                                            fit: BoxFit.cover,
+                                            width: 60,
+                                            height: 60,
+                                          ),
+                                        )),
+                            ),
+                            title: Text(task.title!),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Priority: ${task.priority.toString()}',
+                                ),
+                                Text(
+                                  'Status: ${task.status.toString()}',
+                                ),
+                              ],
+                            ),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                task.status == Status.completed
+                                    ? SizedBox()
+                                    : IconButton(
+                                        icon: Icon(Icons.check),
+                                        onPressed: () {
+                                          task.status = Status.completed;
+                                          _dbHelper.updateTaskStatus(task);
+                                          setState(() {
+                                            _loadTasks();
+                                          });
+                                        },
+                                      ),
+                                IconButton(
+                                  icon: Icon(Icons.delete),
+                                  onPressed: () {
+                                    _deleteTask(task.id!);
+                                    setState(() {
+                                      _loadTasks();
+                                    });
+                                  },
+                                )
+                              ],
+                            ),
+                            onTap: () async {
+                              final result = await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          TaskDetails(currentTask: task)));
+                              if (result == true) {
+                                _loadTasks();
+                              }
+                            },
+                          ),
+                        ),
+                    ]
+                  ],
                 ],
-              ],
-            ),
+              ),
+      ),
     );
   }
 
